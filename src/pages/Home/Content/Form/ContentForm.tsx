7caab9e4206e9a8e5d42ca568e36efb12076ec52
Filipe Leite <filipe.leite@mobiweb.pt>
2019-12-15 16:08:19 +0000

    saving steps state and validating text answers

--- src/pages/Home/Content/Form/ContentForm.tsx
+++ src/pages/Home/Content/Form/ContentForm.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-import { Grid, TextField, Typography, withStyles } from '@material-ui/core';
+import {
+  Grid,
+  TextField,
+  Typography,
+  withStyles,
+  RadioGroup,
+  FormControlLabel,
+  Radio
+} from '@material-ui/core';
 
 import { PagesStep } from '../../../../models/ui/Steps';
 import styles from './ContentForm.styles';
@@ -8,29 +16,72 @@ import styles from './ContentForm.styles';
 interface ContentFormProps {
   classes: any;
   step: PagesStep;
+  handleTextChange: (index: number, value: string) => void;
+  hasBeenChecked: boolean;
 }
 
-const ContentForm = ({ classes, step }: ContentFormProps): JSX.Element => {
+const ContentForm = ({
+  classes,
+  step,
+  handleTextChange,
+  hasBeenChecked
+}: ContentFormProps): JSX.Element => {
   return (
     <Grid item xs={12}>
-      {step.questions.map(
-        (question: { text: string }, index: number) => {
+      {step.questions.map((question: any, index: number) => {
+        if (question.text) {
           return (
             <div key={index} className={classes.questionContainer}>
               <Typography>{question.text}</Typography>
               <TextField
+                key={question.text}
+                error={
+                  hasBeenChecked
+                    ? question.required && question.answer === ''
+                    : false
+                }
+                onChange={event => handleTextChange(index, event.target.value)}
                 className={classes.questionInput}
+                value={question.answer}
                 id="outlined-basic"
                 label={`Questão ${index + 1}`}
                 variant="outlined"
                 multiline
+                required={question.required}
                 rows={4}
                 rowsMax={4}
               />
             </div>
           );
+        } else if (question.multiple) {
+          return (
+            <div key={index} className={classes.questionContainer}>
+              <Typography>{question.multiple.text}</Typography>
+              <RadioGroup
+                aria-label=""
+                name={`question${index}`}
+                value={question.answer}
+                onChange={(event): void =>
+                  handleTextChange(index, event.target.value)
+                }
+              >
+                {question.multiple.options.map(
+                  (option: { id: string; text: string }) => {
+                    return (
+                      <FormControlLabel
+                        key={option.id}
+                        value={option.id}
+                        control={<Radio color="primary" />}
+                        label={option.text}
+                      />
+                    );
+                  }
+                )}
+              </RadioGroup>
+            </div>
+          );
         }
-      )}
+      })}
     </Grid>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,9 +5,7 @@ import React, { ReactNode, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 // Material UI
-import {
-  withStyles
-} from '@material-ui/core';
+import { withStyles } from '@material-ui/core';
 import TopBar from '../../components/TopBar/TopBar';
 import pageStepsContent from '../../Utils/Content/PageStepsContent';
 
@@ -15,6 +13,8 @@ import pageStepsContent from '../../Utils/Content/PageStepsContent';
 import styles from './Home.styles';
 import Content from './Content/Content';
 import LeftBar from '../../components/LeftBar/LeftBar';
+
+import { cloneDeep } from 'lodash';
 
 interface Props extends RouteComponentProps {
   classes: any;
@@ -29,28 +29,65 @@ interface State {
 const Home = ({ classes, title, content }: Props): JSX.Element => {
   const [activeStep, setActiveStep] = useState(0);
 
+  // ESTADO NA MEMORIA É O pageStepsContentState
+  // PODEM USAR ESTE ESTADO PARA GUARDAR EM COOKIES OU NO FICHEIRO CSV
+  const [pageStepsContentState, setPagesStepContent] = useState(
+    pageStepsContent
+  );
+
+  const [hasBeenChecked, setHasBeenChecked] = useState(false);
+
   const handleClickStep = (clickedStep: number): void => {
-    if (true) {
+    let validation = true;
+    activeStep >= 0 &&
+      activeStep <= pageStepsContentState.length - 1 &&
+      pageStepsContentState[activeStep].questions.forEach(question => {
+        if (question.required) {
+          if (question.answer) {
+          } else {
+            validation = false;
+          }
+        } else {
+        }
+      });
+    if (clickedStep < activeStep) {
+      validation = true;
+    }
+
+    if (!validation) {
+      setHasBeenChecked(true);
+    }
+
+    if (validation) {
+      setHasBeenChecked(false);
       setActiveStep(clickedStep);
     }
+  };
+
+  const handleTextChange = (index: number, value: string): void => {
+    const pageStepsContentCopy = cloneDeep(pageStepsContentState);
+
+    pageStepsContentCopy[activeStep].questions[index].answer = value;
+
+    setPagesStepContent(pageStepsContentCopy);
   };
 
   const progressStep = (activeStep / pageStepsContent.length) * 100;
 
   return (
     <div className={classes.root}>
-      <TopBar progress={progressStep}/>
+      <TopBar progress={progressStep} />
       <div className={classes.contentContainer}>
         {
           //@ts-ignore
-          <LeftBar
-            activeStep={activeStep}
-            handleClickStep={handleClickStep}
-          />
+          <LeftBar activeStep={activeStep} handleClickStep={handleClickStep} />
         }
         <Content
-          setActiveStep={setActiveStep}
+          handleTextChange={handleTextChange}
+          pageStepsContent={pageStepsContentState}
+          setActiveStep={handleClickStep}
           activeStep={activeStep}
+          hasBeenChecked={hasBeenChecked}
         />
       </div>
     </div>
